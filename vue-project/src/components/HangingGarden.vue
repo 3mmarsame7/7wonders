6b1c8e8730587ebf3f15152a25086f52aaaf739f
Meram Ehab <miramehab@gmail.com>
2026-08-23 19:18:27 +0300

    Add brand new updates

---
+++ vue-project/src/components/HangingGarden.vue
@@ -0,0 +1,24 @@
+
+<script setup>
+import WonderSection from "./WonderSection.vue";
+import Navbar from "./Navbar.vue";
+import Footer from "./Footer.vue";
+const wonder = { 
+    number: "02",
+    category: "ANCIENT WORLD",
+    title: "HANGING GARDENS OF BABYLON",
+    description:
+        "The Hanging Gardens were described as a remarkable feat of engineering with ascending tiers of gardens.",
+    location: "MAHAWII, IRAQ",
+    built: "c. 600 BCE",
+    image:
+        "https://cdn.britannica.com/39/1139-050-71C43AEE/re-creation-artist-Hanging-Gardens-of-Babylon-world.jpg",
+    theme: "garden",
+};
+</script>
+
+<template>
+    <Navbar />
+    <WonderSection v-bind="wonder" />
+    <Footer />
+</template>
---
+++ vue-project/src/components/LighthouseOfAlexandria.vue
@@ -0,0 +1,25 @@
+[file name]: LighthouseOfAlexandria.vue
+<script setup>
+import WonderSection from "./WonderSection.vue";
+import Navbar from "./Navbar.vue";
+import Footer from "./Footer.vue";
+
+const wonder = {
+    number: "07",
+    category: "ANCIENT WORLD",
+    title: "LIGHTHOUSE OF ALEXANDRIA",
+    description:
+        "One of the Seven Wonders of the Ancient World and for centuries one of the tallest man-made structures.",
+    location: "ALEXANDRIA, EGYPT",
+    built: "c. 280 BCE",
+    image:
+        "https://cdn.britannica.com/07/172307-050-4D3BA855/Pharos-Alexandria.jpg",
+    theme: "sunset",
+};
+</script>
+
+<template>
+    <Navbar />
+    <WonderSection v-bind="wonder" />
+    <Footer />
+</template>
